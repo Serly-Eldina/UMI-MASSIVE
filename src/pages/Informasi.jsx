3cc77b9d8721b2c64f191e50c3f744b224dd7941
shofink <shofi.kamilah28@gmail.com>
2023-12-01 11:31:08 +0700

    revisi navbar page informasi

--- src/pages/Informasi.jsx
+++ src/pages/Informasi.jsx
@@ -17,38 +17,38 @@ const informasi = () => {
 
        
 <NavbarBS expand="lg" className="bg-body-tertiary" sticky='top'>
-  <Container fluid>
-    <NavbarBS.Brand>
-      <img src={logonav} alt='logo' style={{width: '90px', height: '60px', paddingBottom:'20px'}} />
-    </NavbarBS.Brand>
-    <NavbarBS.Toggle aria-controls="navbarScroll" />
-    <NavbarBS.Collapse id="navbarScroll">
-      <Nav
-        className="navbar-custom me-auto my-2 my-lg-0"
-        style={{ maxHeight: 'auto' }}
-        navbarScroll
-      >
-        <Nav.Link href="/">Beranda</Nav.Link>
-        <Nav.Link href="Login">Tentang Kami</Nav.Link>
-        <Nav.Link href="Register">Informasi</Nav.Link>
-        <Nav.Link href="#action2">Konsultasi</Nav.Link>
-      </Nav>
-      <Form className="d-flex" style={{ marginRight: '20px' }}>
-        <Form.Control
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-      <NavbarBS.Brand>
-        <img src={logosementara} alt='logo' />
-      </NavbarBS.Brand>
-    </NavbarBS.Collapse>
-  </Container>
-</NavbarBS>
+        <Container fluid>
+          <NavbarBS.Brand>
+            <img src={logonav} alt='logo' style={{ marginLeft:'20%' , width: '90px', height: '60px', paddingBottom: '10px' }} />
+          </NavbarBS.Brand>
+          <NavbarBS.Toggle aria-controls="navbarScroll" />
+          <NavbarBS.Collapse id="navbarScroll">
+            <Nav
+              className="navbar-custom me-auto me-2 my-2 my-lg-0"
+              style={{ maxHeight: 'auto', marginLeft: '300px' }}
+              navbarScroll
 
+            >
+              <Nav.Link href="/">Beranda</Nav.Link>
+              <Nav.Link href="tentangkami">Tentang Kami</Nav.Link>
+              <Nav.Link href="Informasi">Informasi</Nav.Link>
+              <Nav.Link href="#action2">Konsultasi</Nav.Link>
+            </Nav>
+            <Form className="d-flex" style={{ marginRight: '20px' }}>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+            <NavbarBS.Brand>
+              <img src={logosementara} alt='logo' />
+            </NavbarBS.Brand>
+          </NavbarBS.Collapse>
+        </Container>
+      </NavbarBS>
 
 
 <div className="tips" style={{ backgroundImage: `url(${background})` }}>
