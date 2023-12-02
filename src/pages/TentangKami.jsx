c7a4e221b0e20615279a159fd555b1568e7ec642
Deri Gilang Sumudra <2110631170008@student.unsika.ac.id>
2023-12-03 02:38:10 +0700

    google play add

--- src/pages/TentangKami.jsx
+++ src/pages/TentangKami.jsx
@@ -1,48 +1,49 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
 
 import tentangkami from '../images/tentangkami.png';
 import tentangkami1 from '../images/tentangkami1.png';
 import layanan1 from '../images/layanan1.png';
 import layanan2 from '../images/layanan2.png';
 import layanan3 from '../images/layanan3.png';
-import '../css/tentangkami.css';
+import hape from '../images/hape.png';
+import gplay from '../images/gplay.png'
+
 
 const TentangKami = () => {
     return (
         <>
-           
-            <div>
-                <div>
-                    <img
-                        src={tentangkami}
-                        alt=""
-                        style={{
-                            maxWidth: '100%',
-                            height: 'auto',
-                            display: 'block', // Membuat gambar tetap dalam aliran dokumen
-                            margin: 'auto',   // Pusatkan gambar
-                        }}
-                    />
-                </div>
-                <div className="container1">
-                    <div className="col-md-5">
-                        <div className="gambarlaptop">
-                            <img
-                                src={tentangkami1}
-                                alt="gambar tentang kami"
-                                style={{
-                                    maxWidth: '100%',
-                                    height: 'auto',
-                                    display: 'block',
-                                    margin: 'auto',
-                                }}
-                            />
-                        </div>
-                    </div>
 
-                    <div className="col-md-6">
-                        <div className="teks-tentang-kami">
+            {/* ini awal image header */}
+            <Row>
+                <Col xs={12}>
+                    <Image style={{ width: "100%", height: "auto" }}
+                        src={tentangkami}
+                        className="img-fluid"
+                        alt="header image"
+                    />
+                </Col>
+            </Row>
+            {/* ini akhir image header */}
+
+            {/* ini awal teks tentang kami */}
+            <Container fluid className='mt-4 pt-4'>
+                <Row className="justify-content-center align-items-center">
+                    <Col md={6} className='text-center'>
+                        <img
+                            src={tentangkami1}
+                            alt="Login Image"
+                            className="img-fluid"
+                        />
+                    </Col>
+                    <Col md={6} className='d-flex align-items-center'>
+                        <Container className="m-5">
                             <h3>Tentang Kami</h3>
-                            <p >
+                            <p style={{ textAlign: 'justify' }}>
                                 Selamat datang di platform kami! Kami adalah sebuah aplikasi
                                 mobile dan situs web yang didedikasikan untuk menyediakan sumber
                                 daya informatif seputar tanaman hias. Dengan fokus pada
@@ -50,61 +51,75 @@ const TentangKami = () => {
                                 kami menghadirkan berbagai fitur yang dirancang untuk memenuhi
                                 kebutuhan para penggemar tanaman hias.
                             </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
 
-            <div style={{ paddingTop: '50px' }} className='col-md-12'>
-                <div className='rencana'>
-                    <div className='judul'>
-                        <p>Layanan Yang Anda Dapatkan</p>
+            {/* ini akhir teks tentang kami */}
+
+            {/* ini awalan layanan */}
+
+            <Container>
+                <Row className="justify-content-center"> {/* Added justify-content-center class */}
+                    <Col className='mt-4 pt-4 text-center'> {/* Added text-center class for centering text */}
+                        <h2>Layanan Yang Anda Dapatkan</h2>
                         <p>Ikuti Instruksi untuk lebih lanjut</p>
-                    </div>
+                    </Col>
+                </Row>
+                <Row justify-content-md-center>
+                    <Col sm={4} className='isi1 mt-4 pt-4'>
+                        <img src={layanan1} alt="Layanan 1" className="mb-4" /> {/* Added margin-bottom to create space */}
+                        <p>Platform kami menyajikan berbagai informasi berguna melalui artikel,
+                            tips, video tutorial, dan panduan praktis dalam format yang mudah dipahami.
+                        </p>
+                    </Col>
+                    <Col sm className='isi2 mt-4 pt-4'>
+                        <img src={layanan2} alt='layanan2' className="mb-4" />
+                        <p>Nikmati pengalaman berbagi dan belajar dari sesama pecinta tanaman hias
+                            melalui fitur Konsultasi Anda dapat berpartisipasi dalam diskusi, dengan para pakar.
+                        </p>
 
-                    <div className='isi'>
+                    </Col>
+                    <Col sm className='isi3 mt-4 pt-4'>
+                        <div >
+                            <img src={layanan3} alt='layanan2' className="mb-4" />
+                            <p>Kami memahami bahwa setiap tanaman hias memiliki kebutuhan dan tantangan unik.
+                                Oleh karena itu, kami menyediakan layanan untuk merawat tanaman hias.
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container >
+            {/* ini akhiran layanan */}
 
-                        <div className='col-md-4'>
-                            <div className='isi1'>
-                                <div style={{ width: '18rem' }}>
-                                    <img src={layanan1} />
-                                    <div>
-                                        <div style={{ fontWeight: 'bolder' }}>Penyiraman yang Tepat</div>
-                                        <div className='textrencana' style={{ marginTop: '23px' }}>
-                                            Tentukan jadwal penyiraman berdasarkan jenis tanaman, kondisi iklim, dan musim. Pastikan tanah tidak terlalu kering atau terlalu basah.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> </div>
+            <Container fluid className='mt-4 pt-4'>
+                <Row className="justify-content-center align-items-center">
+                    <Col md lg={{span:5, offsite:1}} className='d-flex align-items-center '>
+                        <Container >
+                            <h3>Dapatkan Versi Aplikasi</h3>
+                            <p style={{ textAlign: 'justify' }}>
+                            Optimalkan pengalaman pengguna Anda dengan mendapatkan versi aplikasi kami sekarang! 
+                            Dengan aplikasi kami, nikmati akses cepat, fitur eksklusif, dan pengalaman yang dioptimalkan, 
+                            memberikan Anda kontrol penuh atas perjalanan digital dilengkapi dengan fitur pengingat untuk 
+                            mengoptimalkan kebutuhan anda. unduh aplikasi kami sekarang!
+                            </p>
+                            <Button variant="dark" size='lg'>
+                               <img src={gplay} alt="" />
+                               
+                            </Button>
+                        </Container>
+                    </Col>
+                    <Col md lg={{span:2, offsite:1}} >
+                        <img
+                            src={hape}
+                            alt="Login Image"
+                            className="img-fluid"
+                        />
+                    </Col>
+                </Row>
+            </Container>
 
-                        <div className='col-md-4'>
-                            <div className='isi2'>
-                                <div style={{ width: '18rem' }}>
-                                    <img src={layanan2} />
-                                    <div>
-                                        <div style={{ fontWeight: 'bolder' }}>Pemupukan Rutin</div>
-                                        <div className='textrencana' style={{ marginTop: '23px' }}>
-                                            Berikan tanaman nutrisi yang diperlukan dengan memilih pupuk yang sesuai dengan jenis tanaman dan fase pertumbuhannya.
-                                        </div>
-                                    </div>
-                                </div></div></div>
-
-                        <div className='col-md-4'>
-                            <div className='isi3'>
-                                <div style={{ width: '18rem' }}>
-                                    <img src={layanan3} />
-                                    <div>
-                                        <div style={{ fontWeight: 'bolder' }}>Perhatikan Pencahayaan</div>
-                                        <div className='textrencana' style={{ marginTop: '23px' }}>
-                                            Beberapa tanaman membutuhkan sinar matahari langsung, dan ada yang di tempat teduh Pahami preferensi pencahayaan tanaman Anda.
-                                        </div>
-                                    </div>
-                                </div></div></div>
-                    </div>
-                </div>
-            </div>
-
-            
         </>
     )
 }
