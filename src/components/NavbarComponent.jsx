a510ce9f17024d2cdaa1c3741110cdc6b318de8d
Deri Gilang Sumudra <2110631170008@student.unsika.ac.id>
2023-12-04 14:18:14 +0700

    nyamain navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,35 +1,26 @@
 
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import NavbarBS from 'react-bootstrap/Navbar';
+import { Navbar, Nav, Button, Container, Form } from 'react-bootstrap';
 
-import logosementara from '../assets/logosementara.png'
-import logonav from '../images/logonav.png'
+import logosementara from '../images/logosementara.png';
+import logonav from '../images/logonav.png';
 
 const NavbarComponent = () => {
   return (
     <div>
-        <NavbarBS expand="lg" className="bg-body-tertiary" sticky='top'>
+      <Navbar expand="lg"  style={{ backgroundColor: 'rgba(48, 113, 86, 0.2)' }}>
         <Container fluid>
-          <NavbarBS.Brand>
-            <img src={logonav} alt='logo' style={{ marginLeft:'20%' , width: '90px', height: '60px', paddingBottom: '10px' }} />
-          </NavbarBS.Brand>
-          <NavbarBS.Toggle aria-controls="navbarScroll" />
-          <NavbarBS.Collapse id="navbarScroll">
-            <Nav
-              className="navbar-custom me-auto me-2 my-2 my-lg-0"
-              style={{ maxHeight: 'auto', marginLeft: '300px' }}
-              navbarScroll
-
-            >
+          <Navbar.Brand>
+            <img src={logonav} alt='logo' style={{ marginLeft: '20%', width: '90px', height: '60px', paddingBottom: '10px' }} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav variant="underline" defaultActiveKey="/home" className="navbar-custom mx-auto me-auto me-2 my-2 my-lg-0" navbarScroll>
               <Nav.Link href="/">Beranda</Nav.Link>
               <Nav.Link href="tentangkami">Tentang Kami</Nav.Link>
               <Nav.Link href="Informasi">Informasi</Nav.Link>
               <Nav.Link href="Konsultasi">Konsultasi</Nav.Link>
             </Nav>
-            <Form className="d-flex" style={{ marginRight: '20px' }}>
+            <Form className="d-flex mb-2 mb-lg-0" style={{ marginRight: '20px' }}>
               <Form.Control
                 type="search"
                 placeholder="Search"
@@ -38,14 +29,14 @@ const NavbarComponent = () => {
               />
               <Button variant="outline-success">Search</Button>
             </Form>
-            <NavbarBS.Brand>
+            <Navbar.Brand className="mt-2 mt-lg-0">
               <img src={logosementara} alt='logo' />
-            </NavbarBS.Brand>
-          </NavbarBS.Collapse>
+            </Navbar.Brand>
+          </Navbar.Collapse>
         </Container>
-      </NavbarBS>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
