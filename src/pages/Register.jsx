14bf19d05304155584d30daba8fb8d24afe1888e
Deri Gilang Sumudra <2110631170008@student.unsika.ac.id>
2023-12-12 05:02:46 +0700

    add back-end login register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,49 +1,40 @@
 
-import { useState, useEffect } from 'react';
+import { useState,  } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import fotologin from '../images/fotologin.png';
 import logologin from '../images/logologin.png';
 import googlelogo from '../images/googlelogo.png'
 
+import validation from './RegisterValidation';
 
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Perform login validation or send data to the server here
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
-
-  useEffect(() => {
-    // Load the Google API for sign-in
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/platform.js';
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      window.gapi.load('auth2', () => {
-        window.gapi.auth2.init({
-          client_id: 'YOUR_GOOGLE_CLIENT_ID',
-        });
-      });
-    };
-  }, []);
-
-  const handleGoogleLogin = () => {
-    const auth2 = window.gapi.auth2.getAuthInstance();
-    auth2.signIn().then((googleUser) => {
-      const profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId());
-      console.log('Name: ' + profile.getName());
-      console.log('Email: ' + profile.getEmail());
-      console.log('Image URL: ' + profile.getImageUrl());
-    });
-  };
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({})
+  const handleInput = (event) => {
+    setValues(prev => ({ ...prev, [event.target.name]: event.target.value }))
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+    const hasErrors = Object.values(errors).some(error => error !== "");
+ 
+    if (!hasErrors) {
+       axios.post('http://localhost:8001/Register', { ...values })
+          .then(res => {
+             navigate('/Login');
+          })
+          .catch(err => console.log(err));
+    }
+ }
+ 
 
   return (
     <Container className="login-container d-flex flex-column align-items-center justify-content-center mt-5">
@@ -66,7 +57,7 @@ const Register = () => {
           <div className="d-flex justify-content-center mb-3">
             <Button
               variant="outline-danger"
-              onClick={handleGoogleLogin}
+              
               className="mb-3 d-flex align-items-center google-login-button">
               <img className='mx-2' style={{height:'25px', width:'25px'}}
                 src={googlelogo}/>
@@ -76,30 +67,30 @@ const Register = () => {
           <div className="text-center">
             <p>atau</p>
           </div>
-          <Form onSubmit={handleLogin}>
-            <Form.Group className="text-start mb-3" controlId="formBasicEmail">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="text-start mb-3" controlId="formBasicName">
               <Form.Control
-                type="name"
+                type="text"
+                name="name"
                 placeholder="Nama Lengkap"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+                onChange={handleInput}/>
+                {errors.name && <span className='text-danger'>{errors.name}</span>}
             </Form.Group>
             <Form.Group className="text-start mb-3" controlId="formBasicEmail">
               <Form.Control
                 type="email"
+                name="email"
                 placeholder="Alamat email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+                onChange={handleInput} />
+              {errors.email && <span className='text-danger'>{errors.email}</span>}
             </Form.Group>
             <Form.Group className="text-start mb-3" controlId="formBasicPassword">
               <Form.Control
                 type="password"
+                name="password"
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+                onChange={handleInput} />
+              {errors.password && <span className='text-danger'>{errors.password}</span>}
             </Form.Group>
             <Button
               variant="secondary"
@@ -115,7 +106,7 @@ const Register = () => {
             </Button>
             {/* Register below the login button */}
             <p className="mt-3 text-center mb-5">
-              Sudah Punya Akun? <a href="#" style={{color:'#34745C'}}>Masuk</a>
+              Sudah Punya Akun? <a href="/Login" style={{color:'#34745C'}}>Masuk</a>
             </p>
           </Form>
         </Col>
